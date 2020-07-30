3e4666920a1770edc2be43c20fc6cecf6b1a0191
Alice Prigent <aliprigent@gmail.com>
2020-07-30 16:11:14 +0200

    apiHandler User

---
+++ src/api/apiUser.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL + '/users',
+  withCredentials: true, 
+});
+
+export default {
+    service,
+  
+    getUsers() {
+      return service
+        .get("/")
+    },
+  
+    getOneUser() {
+      return service
+        .get("/me")
+    },
+
+    updateUser(data) {
+        return service.post("/me", data)
+    }
+  
+  };
+  
