d9a1b668dd43db159d202941f76c82cddc57f288
Cha Noguer <definitely.not.a.chatbot@gmail.com>
2020-07-30 19:05:23 +0200

    prepared new team form - post method untested

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./pages/Profile";
+import FormNewTeam from "./components/Forms/Team/FormNewTeam"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
+        <Route path="/team/create" component={FormNewTeam} />
         <ProtectedRoute exact path="/profile" component={Profile} />
       </Switch>
     </div>
