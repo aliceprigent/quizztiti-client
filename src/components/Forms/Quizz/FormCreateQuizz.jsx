373a50f2a6e53224f9bbffb9f011f47611a8f664
Alice Prigent <aliprigent@gmail.com>
2020-08-03 19:07:17 +0200

    bug

--- src/components/Forms/Quizz/FormCreateQuizz.jsx
+++ src/components/Forms/Quizz/FormCreateQuizz.jsx
@@ -1,10 +1,10 @@
+
 import React, { Component } from "react";
 import quizzHandler from "../../../api/quizzHandler";
 import QuestionBox from "../../Forms/Quizz/QuestionBox";
-
 export class FormCreateQuizz extends Component {
   state = {
-  questionNb: 1,
+    questionNb: 1,
     // incrémenter le compte dès qu'on appuie sur add question
     quizzTotal: [],
     //  push une question dès qu'on appuie sur add question
@@ -38,55 +38,50 @@ export class FormCreateQuizz extends Component {
     event.preventDefault();
     console.log(this.state);
     quizzHandler
-      .createQuizz(this.state)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-
- 
-  render() {
-    return (
-      <div>
-        <form
-          className="quizz-form column"
-          onChange={this.handleChange}
-          onSubmit={this.handleSubmit}
-        >
-          <label htmlFor="title" className="quizz-label">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            className="title quizz-input"
-            placeholder="Short description of your quizz"
-            maxLength="120"
-            defaultValue="mon 1er quizz"
-          />
-          <label htmlFor="thema" className="quizz-label">
-            Topic
-          </label>
-          <select name="thema" className="quizz-select">
+    .createQuizz(this.state)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+render() {
+  return (
+    <div>
+      <form
+        className="quizz-form column"
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit}
+      >
+        <label htmlFor="title" className="quizz-label">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="title quizz-input"
+          placeholder="Short description of your quizz"
+          maxLength="120"
+          defaultValue="mon 1er quizz"
+        />
+        <label htmlFor="thema" className="quizz-label">
+          Topic
+        </label>
+        <select name="thema" className="quizz-select">
           <option value=""></option>
-            <option value="Nature" defaultValue>
-              Nature
-            </option>
-            <option value="General Culture">General Culture</option>
+          <option value="Nature" defaultValue>
+            Nature
+          </option>
+          <option value="General Culture">General Culture</option>
             <option value="Health and Beauty">Health and Beauty</option>
             <option value="Celebrity">Celebrity</option>
             <option value="Society">Society</option>
             <option value="Miscellaneous">Miscellaneous</option>
           </select>
-          
-            
           <label htmlFor="status" className="status">
-          Status
-          </label>
+            Status
+            </label>
           <select name="status" className="quizz-status">
           <option value=""></option>
             <option value="Public" defaultValue>
